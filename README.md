6913a21ea21044517b4c4bb22f8473e8344b8537
Blaine Harper <blaine.harper@indianhills.edu>
2024-06-19 10:25:53 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cloudflare Starter Worker | Preview
+
+A Cloudflare edge hosted Worker for previewing my upcoming book set to realease July 12th. 
---
+++ src/worker.js
@@ -0,0 +1,138 @@
+/**
+ * worker.ts
+ * A Cloudflare edge hosted Worker for Bootstrap UI development.
+ */
+
+//  Import modules
+import { rawHtmlResponse } from './std'
+import { Page } from './dom'
+import { Hono } from 'hono'
+
+var ENV = {
+	siteTitle: 'CF Starter Demo',
+	brand: 'Cloudflare Starter Worker Demo',
+	copyright: 'Blaine Harper',
+	navbar: [{
+		text: 'About',
+		links: [{
+			text: 'Developer',
+			link: '/developer'
+		}, {
+			text: 'Other Projects',
+			link: '/projects'
+		}],
+	}]
+}
+
+// 	set application default's for page generation
+//	Page will use this as the default contents for <head></head> unless overwritten with Page.header
+const _headerDef = `<meta name = "viewport" content = "width=device-width,initial-scale=1"/>
+	<link rel="icon" type="image/x-icon" href="https://blaineharper.com/assets/favicon.ico">
+
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://parking.indianhills.edu/stylesheets/bs.add.css">
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"><\/script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"><\/script>
+	<script src="/js/jQuery.dirty.js"><\/script>
+	<script src="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.13.4/b-2.3.6/b-html5-2.3.6/b-print-2.3.6/datatables.min.js"><\/script>
+	<script src="https://kit.fontawesome.com/5496aaa581.js" crossorigin="anonymous"><\/script>`
+
+const _copyright = `<span id = "footerText"><span id="year"></span> © ${ENV.copyright}</span>
+	<script>document.getElementById("year").innerHTML = new Date().getFullYear();</script>`
+
+const _footerDef = `<div class="mx-auto">
+		<div id="footer_motto" class="mx-auto poh-left-bar p-3 shadow-lg poh-sand bg-gradient text-center panel rounded-0" style="width:15%; min-width:10rem; margin-bottom:7.5rem;">
+			<i>Start your own Cloudflare worker site <a href="https://github.com/bhar2254/Cloudflare-Workers-Starter">here!</a></i>
+		</div>
+		</div >
+	</div >
+	<footer id="mainFooter" class="mx-auto shadow-lg p-2 text-center poh-light-grey bg-gradient sticky-footer">
+		${_copyright}
+	</footer>	
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"><\/script>`
+
+const pageDefaults = {
+	...ENV, 
+	header: _headerDef, 
+	footer: _footerDef,
+}
+Page.setDefs(pageDefaults)
+
+const colors = {
+	'red': { primary: '782F40', secondary: 'FFA400' },
+	'blue': { primary: '001E38', secondary: 'F7C564' },
+	'green': { primary: '01504A', secondary: 'FFC600' },
+	'purple': { primary: '301934', secondary: 'FFA400' },
+	'orange': { primary: 'C04000', secondary: 'F7C564' },
+}
+
+const applyCSSTheme = (scheme) => {
+	const hexToRBG = (hex) => {
+		// Ensure the hex code is exactly 2 digits
+		if (hex.length !== 6) {
+			throw new Error('Invalid hex color format. It should be 6 digits.');
+		}
+		let output = parseInt(hex, 16)
+		output = Math.floor(output)
+		output = Math.min(255, Math.max(0, output))
+		const rDecimalValue = parseInt(hex.substring(0, 2), 16)
+		const gDecimalValue = parseInt(hex.substring(2, 4), 16)
+		const bDecimalValue = parseInt(hex.substring(4, 6), 16)
+
+		// Use the decimal value for each RGB component to create a shade of gray
+		return `${rDecimalValue}, ${gDecimalValue}, ${bDecimalValue}`
+	}
+	if (!Object.keys(colors).includes(scheme))
+		scheme = 'purple'
+	const rbgPrimary = `${hexToRBG(colors[scheme].primary)}`
+	const rbgSecondary = `${hexToRBG(colors[scheme].secondary)}`
+	return `
+		<style>
+		:root{
+			--poh-primary: #${colors[scheme].primary};
+			--poh-primary-rgb: ${rbgPrimary};
+			--poh-secondary: #${colors[scheme].secondary};
+			--poh-secondary-rgb: ${rbgSecondary};
+		}
+		</style>`
+}
+
+const app = new Hono()
+
+//	route handler
+app.get('/', async c => {
+	const page = new Page({
+		page_title: 'Home',
+		headerOverwrite: _headerDef + applyCSSTheme('red'),
+		body: `<div class="p-3 text-center"><h2>Hello World!</h2<</div><br>
+				<img class="p-3 mx-auto d-block rounded" src="https://www.indianhills.edu/favicon.ico" style="max-width:100%; max-height: 25rem">`
+	})
+	return rawHtmlResponse(page.render())
+})
+
+app.get('/developer', async c => {
+	const page = new Page({
+		pageTitle: 'Developer', headerOverwrite: _headerDef + applyCSSTheme('purple'),
+		body: `Hi! My name's Blaine. I make websites and other JavaScript applications. If you're interested in creating your own JavaScript projects like this one, check out my <a href="https://github.com/bhar2254">GitHub</a> or check out my site <a href="https://blaineharper.com">BlaineHarper.com</a> for (possibly?) up to date details.`
+	})
+	return rawHtmlResponse(page.render())
+})
+
+app.get('/projects', async c => {
+	const page = new Page({
+		pageTitle: 'Projects', headerOverwrite: _headerDef + applyCSSTheme('blue'),
+		body: `If you'd like to view our other projects, check out my <a href="https://github.com/Indian-Hills-Community-College">GitHub</a>!`
+	})
+	return rawHtmlResponse(page.render())
+})
+
+app.notFound(c => {
+	const page = new Page({
+		pageTitle: '404 | Not Found!', headerOverwrite: _headerDef + applyCSSTheme('red'),
+		body: `<span class="fs-3">404 Not Found!</span> <hr> PAGE NOT FOUND! Head <a href="/">home</a> to try and find what you're looking for.`
+	})
+	return rawHtmlResponse(page.render())
+})
+
+export default app
